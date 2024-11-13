b23d98e2b5097f7c0129c35ee7891e6cf69cd7d1
minhdang0603 <ruabin1163@gmail.com>
2024-11-13 19:10:07 +0700

    tich hop è

---
+++ Models/Article.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorWeb.Models
+{
+	[Table("post")]
+	public class Article
+	{
+
+		[Key]
+		public string Id { get; set; }
+
+		[StringLength(255)]
+		[Required]
+		[Column(TypeName = "nvarchar")]
+		public string Title { get; set; }
+
+		[Required]
+		[DataType(DataType.Date)]
+		public DateTime Created { get; set; }
+
+		[Column(TypeName = "ntext")]
+		public string Content { get; set; }
+	}
+}
